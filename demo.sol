979363f20850b534de1dbeb138010116e20b781f
ainuryaka <97097119+ainuryaka@users.noreply.github.com>
2023-10-07 03:10:35 +0600

    Update demo.sol

--- demo.sol
+++ demo.sol
@@ -1,30 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Импортируем стандарт для ERC20 токена
+// Import the ERC20 interface
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TokenTransfer {
-    // Адрес ERC20 токена
+    // Address of the ERC20 token
     address public tokenAddress;
 
-    // Конструктор, принимающий адрес ERC20 токена
+    //Constructor that accepts the ERC20 token address
     constructor(address _tokenAddress) {
         tokenAddress = _tokenAddress;
     }
 
-    // Функция для отправки токенов на другой счет
+    // Function to send tokens to another account
     function transferTokens(address to, uint256 amount) external {
-        // Создаем экземпляр ERC20 токена
+        // Create an instance of the ERC20 token
         IERC20 token = IERC20(tokenAddress);
 
-        // Передаем токены с текущего счета на указанный счет
+        // Transfer tokens from the current account to the specified account
         require(token.transfer(to, amount), "Transfer failed");
     }
 }
 
 // contract UserBalanceChecker {
-//     // Функция для получения баланса пользователя
+//     // Function to get user balance
 //     function getUserBalance(address user) public view returns (uint256) {
 //         return user.balance;
 //     }
